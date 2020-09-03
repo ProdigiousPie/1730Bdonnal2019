047090ab561c205df358454d2c65a4b810829a39
Brendan Donnal <ye4559gl@go.minnstate.edu>
2020-09-03 14:59:04 -0500

    Completed ex1c

---
+++ Bdonnal1c1/frmfoodtruck.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bdonnal1c1
+{
+    public partial class frmfoodtruck : Form
+    {
+        public frmfoodtruck()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHotDogs_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtHotDogsSubtotal.Text = (
+                4.0m * Convert.ToDecimal(txtHotDogs.Text)
+                ).ToString("0.00");
+           txtHamburgersSubtotal.Text = (
+                  5.0m * Convert.ToDecimal(txtHamburgers.Text)
+                ).ToString("0.00");
+            txtPretaxTotal.Text = (
+                (Convert.ToDecimal(txtHotDogsSubtotal.Text) + Convert.ToDecimal(txtHamburgersSubtotal.Text)).ToString("0.00")
+                );
+            TxtTax.Text = (
+                 0.06875m * Convert.ToDecimal(txtPretaxTotal.Text)
+                ).ToString("0.00");
+            TxtTotal.Text = (
+                (Convert.ToDecimal(TxtTax.Text) + Convert.ToDecimal(txtPretaxTotal.Text))
+                ).ToString("0.00");
+
+            btnClear.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHotDogs.Text = "0";
+            txtHamburgers.Text = "0";
+            txtHotDogsSubtotal.Text = "0";
+            txtHamburgersSubtotal.Text = "0";
+            txtPretaxTotal.Text = "0";
+            TxtTax.Text = "0";
+            TxtTotal.Text = "0";
+
+            txtHotDogs.Focus();
+        }
+    }
+}
